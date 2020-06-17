cd7e18eeadceeff6b28bc40d48d9644d48354dd1
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2020-06-17 18:31:07 +0800

    Stripped to base necessities

--- public/main.js
+++ public/main.js
@@ -1,37 +1,22 @@
-const electron = require('electron');
-const ipcMain = electron.ipcMain;
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {app, ipcMain, BrowserWindow} = require('electron');
 
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
-let imageWindow;
-let settingsWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: { webSecurity: false}});
-  imageWindow = new BrowserWindow({width: 600, height: 600, parent: mainWindow, show: false});
-  settingsWindow = new BrowserWindow({width: 600, height: 600, parent: mainWindow, show: false});
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 440, 
+    webPreferences: { webSecurity: false},
+    frame: false,
+  });
 
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  imageWindow.loadURL(isDev ? 'http://localhost:3000/image' : `file://${path.join(__dirname, '../build/index.html')}`);
-  settingsWindow.loadURL(isDev ? 'http://localhost:3000/settings' : `file://${path.join(__dirname, '../build/index.html')}`);
-
-
+  mainWindow.webContents.openDevTools({mode:'detach'})
   mainWindow.on('closed', () => mainWindow = null);
 
-  imageWindow.on('close', (e) => {
-    e.preventDefault();
-    imageWindow.hide();
-  });
-
-  settingsWindow.on('close', (e) => {
-    e.preventDefault();
-    settingsWindow.hide();
-  });
 }
 
 app.on('ready', createWindow);
@@ -41,19 +26,3 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-ipcMain.on('toggle-image', (event, arg) => {
-  imageWindow.show();
-  imageWindow.webContents.send('image', arg);
-})
-
-
-ipcMain.on('toggle-settings', () => {
-  settingsWindow.isVisible() ? settingsWindow.hide() : settingsWindow.show();
-})
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './global.scss';
 import registerServiceWorker from './registerServiceWorker';
-import Router from './Router';
+import App from './App';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+, document.getElementById('root'));
 registerServiceWorker();
